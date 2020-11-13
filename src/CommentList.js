d3831f726402232373d5212b9b461d65af8f9379
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-14 07:54:42 +1100

    showing diferent content according to the event status of comment

--- src/CommentList.js
+++ src/CommentList.js
@@ -7,7 +7,21 @@ import React, { useState, useEffect } from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    //check the status of comment, change it according to the status type of the received event. if rejected -> dont show the comment, show something else
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = " ⏳ This Comment awaits admin approval";
+    }
+
+    if (comment.status === "rejected") {
+      content = " ☠️ This Comment is rejected due to inappropriate conetent";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return (
